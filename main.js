846fb1465c5c646e12be2081075cfe8a7abb1e69
LassassinX <sanjid8426@gmail.com>
2023-02-08 14:54:07 +0600

    added canon.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { LOAD_GARAGE, LOAD_CARS } from './assets_exporter.js';
+import { LOAD_GARAGE, LOAD_CARS } from './utils/assets_exporter.js';
+import { MAKE_SPOTLIGHT } from './utils/util.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -23,67 +24,50 @@ animate()
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap; 
 
-
-const MAKE_SPOTLIGHT = function ([x, y, z],intensity) {
-	const spotLight = new THREE.SpotLight(0xffffff, intensity); 
-	spotLight.penumbra = 0.5;
-	scene.add(spotLight);
-	const spotLightHelper = new THREE.SpotLightHelper(spotLight);
-	scene.add(spotLightHelper);
-	spotLight.castShadow = true;
-
-	spotLight.shadow.mapSize.width = 1024 * 2;
-	spotLight.shadow.mapSize.height = 1024 * 2;
-	//rotate spotlight
-	spotLight.position.set(x, y, z);
-	spotLight.target.position.set(0, 0, 0);
-	scene.add(spotLight.target);
-}
-
-
-MAKE_SPOTLIGHT([0, 20, -19], 0.5)
-MAKE_SPOTLIGHT([0, 20, 20], 0.2)
-
-LOAD_GARAGE({
-	path: 'assets/warehouse.glb',
-	transforms: {
-		scale: [0.04, 0.04, 0.04],
-	},
-}, scene)
-
 // add axis
 const axesHelper = new THREE.AxesHelper(500);
 axesHelper.position.set(0, 3, 0);
 scene.add(axesHelper);
 
-LOAD_CARS([
-	{
-		path: 'assets/cars/ferzor.glb',
-		transforms: {
-			position: [2, 0.2, 1],
-			rotation: [0, 70, 0],
-		}
-	},
-	{
-		path: 'assets/cars/senna_2018.glb',
-		transforms: {
-			position: [-2, 0.2, 0], 
-			rotation: [0, 15, 0],
-		} 
-	},
-	{
-		path: 'assets/cars/f50.glb',
-		transforms: {
-			position: [2, 0.2, -6],
-			rotation: [0, 15, 0],
-			scale: [1.1, 1.1, 1.1],
-		}
-	}, {
-		path: 'assets/cars/centenario_rs.glb',
-		transforms: {
-			position: [-2, 0.2, -6],
-			rotation: [0, 15, 0],
-			scale: [1.2, 1.2, 1.2],
-		}
-	}], scene)
+MAKE_SPOTLIGHT([0, 20, -19], 0.5)
+MAKE_SPOTLIGHT([0, 20, 20], 0.2)
+
+// LOAD_GARAGE({
+// 	path: 'assets/garage.glb',
+// 	transforms: {
+// 		scale: [0.04, 0.04, 0.04],
+// 	},
+// }, scene)
+
+
+// LOAD_CARS([
+// 	{
+// 		path: 'assets/cars/ferzor.glb',
+// 		transforms: {
+// 			position: [2, 0.2, 1],
+// 			rotation: [0, 70, 0],
+// 		}
+// 	},
+// 	{
+// 		path: 'assets/cars/senna_2018.glb',
+// 		transforms: {
+// 			position: [-2, 0.2, 0], 
+// 			rotation: [0, 15, 0],
+// 		} 
+// 	},
+// 	{
+// 		path: 'assets/cars/f50.glb',
+// 		transforms: {
+// 			position: [2, 0.2, -6],
+// 			rotation: [0, 15, 0],
+// 			scale: [1.1, 1.1, 1.1],
+// 		}
+// 	}, {
+// 		path: 'assets/cars/centenario_rs.glb',
+// 		transforms: {
+// 			position: [-2, 0.2, -6],
+// 			rotation: [0, 15, 0],
+// 			scale: [1.2, 1.2, 1.2],
+// 		}
+// 	}], scene)
 
---
+++ utils/util.js
@@ -0,0 +1,17 @@
+const MAKE_SPOTLIGHT = function ([x, y, z],intensity) {
+	const spotLight = new THREE.SpotLight(0xffffff, intensity); 
+	spotLight.penumbra = 0.5;
+	scene.add(spotLight);
+	const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+	scene.add(spotLightHelper);
+	spotLight.castShadow = true;
+
+	spotLight.shadow.mapSize.width = 1024 * 2;
+	spotLight.shadow.mapSize.height = 1024 * 2;
+	//rotate spotlight
+	spotLight.position.set(x, y, z);
+	spotLight.target.position.set(0, 0, 0);
+	scene.add(spotLight.target);
+}
+
+export { MAKE_SPOTLIGHT }
