9be9a87c372493727b5c1fd4566f4ecef4a5a450
Ahmed Saad Tanim <55749602+AhmedSaadTanim@users.noreply.github.com>
2023-02-08 15:18:58 +0600

    adding scene asset

---
+++ js/constants.js
@@ -0,0 +1,13 @@
+// CONSTANTS
+
+// CAMERA
+let FOV = 65
+let ASPECT_RATIO = window.innerWidth / window.innerHeight
+let NEAR = 1
+let FAR = 1000
+
+export default {
+	CAMERA:{
+		FOV, ASPECT_RATIO, NEAR, FAR,
+	} 
+}
