f9ac5106b7c8ba6e5d68f7750d9facf1c19c89a3
LassassinX <sanjid8426@gmail.com>
2023-02-13 11:47:46 +0600

    car can now be driven under senna test drive

--- main.js
+++ main.js
@@ -2,30 +2,68 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { LOAD_CAR, LOAD_GARAGE, REMOVE_ALL_CARS } from './utils/assets_exporter.js';
 import { MAKE_SPOTLIGHT } from './utils/util.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+import * as CANNON from 'cannon-es';
+import CannonDebugger from 'cannon-es-debugger';
+
+// initiate 3js world
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-// const controls = new OrbitControls(camera, renderer.domElement);
+
+// new OrbitControls(camera, renderer.domElement);
+
 renderer.outputEncoding = THREE.sRGBEncoding  //gamma correction
 const initial_camera_props = {
-	position: {x: -11.865113888411006, y: 1.1325965727864835, z: -7.166462717091821},
-	rotation: {_x: -2.9924316504531876, _y: -0.8544188815908706, _z: -3.0287367822776},  
+	position: { x: -11.865113888411006, y: 1.1325965727864835, z: -7.166462717091821 },
+	rotation: { _x: -2.9924316504531876, _y: -0.8544188815908706, _z: -3.0287367822776 },
 }
 
-const animate = function () {
-	requestAnimationFrame(animate);
-	renderer.render(scene, camera);
-	camera.updateProjectionMatrix() 
-}
-// let initial_camera_position = [-8.575898669016357,2.7334088950337563,-8.862992367247971]
-// camera.position.set(...initial_camera_position)
+
 camera.position.set(initial_camera_props.position.x, initial_camera_props.position.y, initial_camera_props.position.z)
-camera.rotation.set(initial_camera_props.rotation._x, initial_camera_props.rotation._y, initial_camera_props.rotation._z) 
+camera.rotation.set(initial_camera_props.rotation._x, initial_camera_props.rotation._y, initial_camera_props.rotation._z)
 
 camera.updateProjectionMatrix()
 camera.updateMatrixWorld(true)
+
+window.addEventListener('resize', () => {
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+})
+
+// initiate cannon world
+const world = new CANNON.World({
+	gravity: new CANNON.Vec3(0, -9.82, 0),
+});
+
+const ground = new CANNON.Body({
+	type: CANNON.Body.STATIC,
+	shape: new CANNON.Plane(),
+	material: new CANNON.Material({ friction: 2, restitution: 0.05 }),
+});
+
+
+ground.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+
+world.addBody(ground);
+
+// const cannonDebugger = new CannonDebugger(scene, world);
+
+const animate = function () {
+	requestAnimationFrame(animate);
+
+	// 3js
+	renderer.render(scene, camera);
+	camera.updateProjectionMatrix()
+
+	// cannon
+	world.fixedStep();
+	// cannonDebugger.update();
+}
 animate()
 
 //enable shadows
@@ -48,27 +86,27 @@ const ambientLight = new THREE.AmbientLight(0xffffff, 0.03);
 scene.add(ambientLight);
 
 LOAD_GARAGE({
-	path: 'assets/garage.glb',  
+	path: 'assets/garage.glb',
 	transforms: {
-		scale: [0.013, 0.013, 0.013], 
+		scale: [0.013, 0.013, 0.013],
 	},
 }, scene)
 
-const cars =  [
+const cars = [
 	{
-		name: 'Ferzor', 
+		name: 'Ferzor',
 		path: 'assets/cars/ferzor.glb',
 		transforms: {
 			rotation: [0, 3, 0],
-			position: [-8, 0, -4], 
+			position: [-8, 0, -4],
 		}
-	}, 
+	},
 	{
 		name: 'centenario',
 		path: 'assets/cars/centenario_rs.glb',
 		transforms: {
 			rotation: [0, -1.7, 0],
-			position: [-8.6, 0, -4.8],  
+			position: [-8.6, 0, -4.8],
 		}
 	},
 	{
@@ -76,7 +114,7 @@ const cars =  [
 		path: 'assets/cars/f50.glb',
 		transforms: {
 			rotation: [0, -1.75, 0],
-			position: [-8.6, 0, -4.8],  
+			position: [-8.6, 0, -4.8],
 		}
 	},
 	{
@@ -84,7 +122,17 @@ const cars =  [
 		path: 'assets/cars/senna_2018.glb',
 		transforms: {
 			rotation: [0, -1.7, 0],
-			position: [-7.7, 0, -4.4], 
+			position: [-7.7, 0, -4.4],
+		},
+		drive_path: 'assets/cars/wheelless_senna.glb',
+		wheel_path: 'assets/cars/senna_wheel.glb'
+	},
+	{
+		name: 'test',
+		path: 'assets/cars/test.glb',
+		transforms: {
+			rotation: [0, -1.7, 0],
+			position: [-7.7, 0, -4.4],
 		}
 	}
 ]
@@ -93,20 +141,206 @@ const cars =  [
 const car_list = document.getElementById('car-selection')
 car_list.addEventListener('change', (e) => {
 	let selected_car = cars.find(car => car.name.toLowerCase() === e.target.value.toLowerCase())
-	REMOVE_ALL_CARS(scene) 
-	LOAD_CAR(selected_car, scene) 
+	REMOVE_ALL_CARS(scene)
+	LOAD_CAR(selected_car, scene)
 })
 
-car_list.dispatchEvent(new Event('change')) 
+car_list.dispatchEvent(new Event('change'))
 
-// LOAD_CARS([
-// 	{
-// 		name: 'Ferzor', 
-// 		path: 'assets/cars/ferzor.glb',
-// 		transforms: {
-// 			rotation: [0, 3, 0],
-// 			position: [-8, 0, -4], 
-// 		}
-// 	}], scene) 
 
- 
+
+document.getElementById('test-drive').addEventListener('click', async () => {
+	scene.remove(scene.getObjectByName('garage'))
+	const car_props = scene.getObjectByName('car ' + car_list.value)
+	let car_path = car_props.userData.drive_path
+	let wheel_path = car_props.userData.wheel_path
+	console.log(car_path)
+	console.log(wheel_path)
+	scene.remove(car_props)
+	document.getElementById('ui-screen-intro').style.display = 'none'
+
+	// add cannon car
+	const car_chassis = new CANNON.Body({
+		mass: 950,
+		position: new CANNON.Vec3(0, 6, 0),
+		shape: new CANNON.Box(new CANNON.Vec3(3.1, 0.64, 1.4)),
+	})
+
+	const vehicle = new CANNON.RigidVehicle({
+		chassisBody: car_chassis
+	})
+
+	const axisWidth = 2.3
+
+	let wheelPositions = [
+		new CANNON.Vec3(-1.68, -.4, axisWidth / 2),
+		new CANNON.Vec3(-1.68, -.4, -axisWidth / 2),
+		new CANNON.Vec3(2.02, -.4, axisWidth / 2),
+		new CANNON.Vec3(2.02, -.4, -axisWidth / 2),
+	];
+
+	// wheels
+	for (let i = 0; i < 4; i++) {
+		vehicle.addWheel({
+			body: generateWheelBody(),
+			position: wheelPositions[i],
+			axis: new CANNON.Vec3(0, 0, 1),
+			direction: new CANNON.Vec3(0, -1, 0), // direction the wheel is facing
+		})
+	}
+	vehicle.addToWorld(world)
+
+	document.addEventListener('keydown', (e) => {
+		const speed = vehicle.chassisBody.velocity.length();
+		let maxSteerVal = Math.PI / 16
+
+		const maxForce = 3000
+
+		switch (e.key) {
+			case 'w':
+				vehicle.setWheelForce(maxForce, 0)
+				vehicle.setWheelForce(maxForce, 1)
+				break
+			case 's':
+				vehicle.setWheelForce(-maxForce, 0)
+				vehicle.setWheelForce(-maxForce, 1)
+				break
+			case 'a':
+				vehicle.setSteeringValue(maxSteerVal, 0)
+				vehicle.setSteeringValue(maxSteerVal, 1)
+				break
+			case 'd':
+				vehicle.setSteeringValue(-maxSteerVal, 0)
+				vehicle.setSteeringValue(-maxSteerVal, 1)
+				break
+		}
+	})
+
+	document.addEventListener('keyup', (e) => {
+		switch (e.key) {
+			case 'w':
+				vehicle.setWheelForce(0, 0)
+				vehicle.setWheelForce(0, 1)
+				break
+			case 's':
+				vehicle.setWheelForce(0, 0)
+				vehicle.setWheelForce(0, 1)
+				break
+			case 'a':
+				vehicle.setSteeringValue(0, 0)
+				vehicle.setSteeringValue(0, 1)
+				break
+			case 'd':
+				vehicle.setSteeringValue(0, 0)
+				vehicle.setSteeringValue(0, 1)
+				break
+		}
+	})
+
+	let car_wheel_list = []
+
+	let loader = new GLTFLoader()
+	let car;
+
+	await loader.load(car_path, (gltf) => {
+		car = gltf.scene
+		car.name = 'car ' + car_list.value
+		scene.add(gltf.scene)
+
+		loader.load(wheel_path, (gltf) => {
+			const wheel1 = gltf.scene
+			wheel1.name = 'FL'
+			scene.add(gltf.scene)
+
+			const wheel2 = gltf.scene.clone()
+			wheel2.name = 'FR'
+			scene.add(wheel2)
+
+			const wheel3 = gltf.scene.clone()
+			wheel3.name = 'RL'
+			scene.add(wheel3)
+
+			const wheel4 = gltf.scene.clone()
+			wheel4.name = 'RR'
+			scene.add(wheel4)
+
+			car_wheel_list.push(wheel1)
+			car_wheel_list.push(wheel2)
+			car_wheel_list.push(wheel3)
+			car_wheel_list.push(wheel4)
+		})
+	})
+
+
+
+	let radius = 10;
+	let theta = Math.PI / 4;
+	let phi = Math.PI / 4;
+	let mousedown = false;
+
+	document.onmousedown = () => {
+		mousedown = true;
+	};
+
+	document.onmouseup = () => {
+		mousedown = false;
+	};
+
+	document.onmousemove = (e) => {
+		if (!mousedown) return;
+		const x = e.clientX;
+		const y = e.clientY;
+		const width = window.innerWidth;
+		const height = window.innerHeight;
+		const dx = x - width / 2;
+		const dy = y - height / 2;
+		theta = Math.PI / 4 + dx / width * Math.PI / 2;
+		phi = Math.PI / 4 + dy / height * Math.PI / 2;
+	};
+
+	const func = () => {
+		if (car && car_wheel_list.length === 4) {
+			car.position.copy(car_chassis.position)
+			car.position.y -= 0.86
+			car.quaternion.copy(car_chassis.quaternion)
+			const offset = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), -Math.PI / 2);
+			car.quaternion.multiply(offset);
+
+			car_wheel_list.forEach((wheel, i) => {
+				wheel.position.copy(vehicle.wheelBodies[i].position)
+				wheel.quaternion.copy(vehicle.wheelBodies[i].quaternion)
+			})
+
+			// follow camera
+
+			camera.position.x = car.position.x + radius * Math.sin(phi) * Math.cos(theta);
+			camera.position.y = car.position.y + radius * Math.cos(phi);
+			camera.position.z = car.position.z + radius * Math.sin(phi) * Math.sin(theta);
+
+			camera.lookAt(car.position);
+		}
+
+		requestAnimationFrame(func)
+		// console.log(vehicle) 
+	}
+	func()
+
+
+
+})
+
+
+function generateWheelBody() {
+	const wheelShape = new CANNON.Sphere(0.5)
+	const wheelMaterial = new CANNON.Material({ friction: 2, restitution: 0.01 })
+	// what is restituion?
+	// https://en.wikipedia.org/wiki/Coefficient_of_restitution
+
+	const wheelBody = new CANNON.Body({ mass: 12, material: wheelMaterial })
+	wheelBody.addShape(wheelShape)
+	wheelBody.angularDamping = 0.5
+	// set friction of wheels 
+	wheelBody.friction = 10
+	return wheelBody
+}
+
--- utils/assets_exporter.js
+++ utils/assets_exporter.js
@@ -1,9 +1,12 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { FBXLoader  } from 'three/addons/loaders/FBXLoader.js'; 
+
 const loader = new GLTFLoader();
 
 const LOAD_GARAGE = ({ path, transforms }, scene) => {
 	loader.load(path, function (gltf) {
+		gltf.scene.name = 'garage'
 		scene.add(gltf.scene);
 		setTransforms(gltf.scene, transforms)
 
@@ -18,27 +21,70 @@ const LOAD_GARAGE = ({ path, transforms }, scene) => {
 	})
 }
 const REMOVE_ALL_CARS = (scene) => { 
-	scene.children = scene.children.filter(child => !child.name.includes('Car')) 
+	scene.children = scene.children.filter(child => !child.name.includes('car')) 
 	// console.log(scene.children) 
 }
-const LOAD_CAR = (car, scene) => {
+const LOAD_CAR = (car, scene, lookAt = null) => {
+	if (car.path.includes('.fbx')) {
+		let loader = new FBXLoader() 
+		loader.load(car.path, function (fbx) {
+			const gui = new dat.GUI() 
+			fbx.scene.name = 'car ' +car.name.toLowerCase()
+			scene.add(fbx.scene);
+ 
+			const cubeFolder = gui.addFolder(car.name)
+			// //add rotation in degrees
+			cubeFolder.add(fbx.scene.rotation, 'x', -10, 10).step(0.01)
+			cubeFolder.add(fbx.scene.rotation, 'y', -10, 10).step(0.01)
+			cubeFolder.add(fbx.scene.rotation, 'z', -10, 10).step(0.01) 
+
+			// //add position
+			cubeFolder.add(fbx.scene.position, 'x', -10, 10).step(0.1)
+			cubeFolder.add(fbx.scene.position, 'y', -10, 10).step(0.1)
+			cubeFolder.add(fbx.scene.position, 'z', -10, 10).step(0.1)
+			
+			// //add scale
+			cubeFolder.add(fbx.scene.scale, 'x', -10, 10).step(0.1)
+			cubeFolder.add(fbx.scene.scale, 'y', -10, 10).step(0.1)
+			cubeFolder.add(fbx.scene.scale, 'z', -10, 10).step(0.1) 
+
+			setTransforms(fbx.scene, car.transforms)
+			//enable shadows
+			fbx.scene.traverse(function (child) {
+				if (child.isMesh) {
+					// child.receiveShadow = true;
+					child.castShadow = true;
+				}
+			});
+		})
+
+		return;
+	}
 	loader.load(car.path, function (gltf) {
 		const gui = new dat.GUI() 
-		gltf.scene.name = 'Car ' +car.name
+		gltf.scene.name = 'car ' +car.name.toLowerCase()
+		if(car.drive_path){
+			gltf.scene.userData.drive_path = car.drive_path
+		}
+
+		if(car.wheel_path){
+			gltf.scene.userData.wheel_path = car.wheel_path
+		}
+		
 		scene.add(gltf.scene);
 
 		// const cubeFolder = gui.addFolder(car.name)
-		// //add rotation in degrees
+		// // //add rotation in degrees
 		// cubeFolder.add(gltf.scene.rotation, 'x', -10, 10).step(0.01)
 		// cubeFolder.add(gltf.scene.rotation, 'y', -10, 10).step(0.01)
 		// cubeFolder.add(gltf.scene.rotation, 'z', -10, 10).step(0.01) 
 
-		// //add position
+		// // //add position
 		// cubeFolder.add(gltf.scene.position, 'x', -10, 10).step(0.1)
 		// cubeFolder.add(gltf.scene.position, 'y', -10, 10).step(0.1)
 		// cubeFolder.add(gltf.scene.position, 'z', -10, 10).step(0.1)
 		
-		// //add scale
+		// // //add scale
 		// cubeFolder.add(gltf.scene.scale, 'x', -10, 10).step(0.1)
 		// cubeFolder.add(gltf.scene.scale, 'y', -10, 10).step(0.1)
 		// cubeFolder.add(gltf.scene.scale, 'z', -10, 10).step(0.1) 
